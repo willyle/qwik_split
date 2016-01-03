335542a70274c5200424b73bb13c3238b9208446
William Le <willy.le.dev@gmail.com>
2016-01-03 18:43:29 -0500

    Created sessions controller with log in and log out abilities.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	@user = User.authenticate(params[:login], params[:password])
+
+	if @user
+		session[:user_id] = @user.id
+		flash[:notice] = "You have logged in successfully."
+		redirect_to dashboard_path
+	else
+		flash[:alert] = "Your information was incorrect."
+		redirect_to login_path
+	end
   end
 
   def destroy
+  	session[:user_id] = nil
+   flash[:notice] = "You have signed out"
+   redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
